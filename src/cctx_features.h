2e797c3f9dd9b39d42796d9ffc54cd88575edff4
domalb <domenico.albani@osome-studio.com>
2017-12-30 07:05:13 +0100

    features

--- src/cctx_features.h
+++ src/cctx_features.h
@@ -15,51 +15,89 @@
 //	CCTX_FEATURES_CPP_ALIGN_OF
 //	CCTX_FEATURES_CPP_COUNTER
 
-#if ((defined __cpp_exceptions) || (defined __EXCEPTIONS) || \
-	 ((defined __clang__) && (__has_feature(cxx_exceptions))) || \
-	 ((defined  _MSC_VER) && (defined _CPPUNWIND)))
+// Exceptions
+#if ((defined __cpp_exceptions) || (defined __EXCEPTIONS))
 #	define CCTX_FEATURES_CPP_EXCEPTIONS
+#elif (defined __clang__)
+#	if (__has_feature(cxx_exceptions))
+#		define CCTX_FEATURES_CPP_EXCEPTIONS
+#	endif
+#elif (defined _MSC_VER)
+#	if (defined _CPPUNWIND)
+#		define CCTX_FEATURES_CPP_EXCEPTIONS
+#	endif
 #endif
 
-#if ((defined __cpp_rtti) || \
-	 ((defined __clang__) && (__has_feature(cxx_rtti))) || \
-	 ((defined  __GCC__) && (defined __GXX_RTTI)) || \
-	 ((defined  _MSC_VER) && (defined _CPPRTTI)) || \
-	 ((defined __IBMCPP__) && (!defined __NO_RTTI__)))
+// RTTI
+#if (defined __cpp_rtti)
 #	define CCTX_FEATURES_CPP_RTTI
+#elif (defined __clang__)
+#	if (__has_feature(cxx_rtti))
+#		define CCTX_FEATURES_CPP_RTTI
+#	endif
+#elif (defined __GCC__)
+#	if (defined __GXX_RTTI)
+#		define CCTX_FEATURES_CPP_RTTI
+#	endif
+#elif (defined __IBMCPP__)
+#	if (!defined __NO_RTTI__)
+#		define CCTX_FEATURES_CPP_RTTI
+#	endif
+#elif (defined _MSC_VER)
+#	if (defined _CPPRTTI)
+#		define CCTX_FEATURES_CPP_RTTI
+#	endif
 #endif
 
+// R-value ref
 #if ((defined __cpp_rvalue_references) || \
-	 ((defined __clang__) && (__has_extension(cxx_rvalue_references))) || \
 	 ((defined _MSC_VER) && (_MSC_VER >= 1600)))
 #	define CCTX_FEATURES_CPP_RVALUE_REF
+#elif (defined __clang__)
+#	if (__has_extension(cxx_rvalue_references))) || \
+#		define CCTX_FEATURES_CPP_RVALUE_REF
+#	endif
 #endif
 
-#if ((defined __cpp_constexpr) || \
-	 ((defined __clang__) && (__has_feature(cxx_constexpr))))
+// Const expressions
+#if (defined __cpp_constexpr)
 #	define CCTX_FEATURES_CPP_CONST_EXP
+#elif (defined __clang__)
+#	if (__has_feature(cxx_constexpr))
+#		define CCTX_FEATURES_CPP_CONST_EXP
+#	endif
 #endif
 
 #if (defined __cpp_if_constexpr)
 #	define CCTX_FEATURES_CPP_IF_CONST_EXP
 #endif
 
+// Static assert
 #if ((defined __cpp_static_assert))
 #	define CCTX_FEATURES_CPP_STATIC_ASSERT
 #endif
 
-#if ((0) || \
-	 ((defined __clang__) && (__has_feature(cxx_alignas))))
+// Align as
+#if (defined __alignas_is_defined)
 #	define CCTX_FEATURES_CPP_ALIGN_AS
+#elif (defined __clang__)
+#	if (__has_feature(cxx_alignas))
+#		define CCTX_FEATURES_CPP_ALIGN_AS
+#	endif
 #endif
 
-#if ((0) || \
-	 ((defined __clang__) && (__has_feature(cxx_alignof))) || \
-	 ((defined _MSC_VER) && (__cplusplus > 201103L)))
-#	define CCTX_FEATURES_CPP_ALIGN_OF
-#elif ((defined _MSC_VER) && (_MSC_VER >= 1400))
-#	define CCTX_FEATURES_CPP_ALIGN_OF
-#	define alignof __alignof
+// Align of
+#if (defined __clang__)
+#	if (__has_feature(cxx_alignof))
+#		define CCTX_FEATURES_CPP_ALIGN_OF
+#	endif
+#elif (defined _MSC_VER)
+#	 if (__cplusplus > 201103L)
+#		define CCTX_FEATURES_CPP_ALIGN_OF
+#	elif (_MSC_VER >= 1400)
+#		define CCTX_FEATURES_CPP_ALIGN_OF
+#		define alignof __alignof
+#	endif
 #elif ((defined __GNUC__) && _CCTX_GCC_VERSION_MIN(4, 2, 1))
 #	define CCTX_FEATURES_CPP_ALIGN_OF
 #	define alignof __alignof__
